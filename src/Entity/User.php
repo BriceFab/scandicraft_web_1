b0304898c1be9bc680e566eee56adec8dbdb3914
BriceFab <bricefab123@gmail.com>
2020-03-22 20:40:45 +0100

    bootstrap grid + register form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,7 +64,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return $this->username;
+        return (string) $this->username;
     }
 
     /**
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,19 +20,9 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'required' => true,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -37,12 +30,49 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 256,
                     ]),
                 ],
             ])
-        ;
+            ->add('username', TextType::class, [
+                'label' => 'Pseudonyme',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 15,
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'mapped' => false,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 256,
+                    ]),
+                ],
+            ]);
+        // ->add('agreeTerms', CheckboxType::class, [
+        //     'mapped' => false,
+        //     'constraints' => [
+        //         new IsTrue([
+        //             'message' => 'You should agree to our terms.',
+        //         ]),
+        //     ],
+        // ])
     }
 
     public function configureOptions(OptionsResolver $resolver)
