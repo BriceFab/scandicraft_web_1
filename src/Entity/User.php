6d64d3f8b61484dc207f963e5f168d7c25587a16
BriceFab <bricefab123@gmail.com>
2020-03-23 14:59:04 +0100

    form global errors

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface
 {
