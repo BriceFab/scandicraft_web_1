680ef63f421a67a8b54acb73f9b466a44c84c57e
BriceFab <bricefab123@gmail.com>
2020-03-23 18:36:34 +0100

    react + toastify + login error toast

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/global.scss';
+import '../css/ReactToastify.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
