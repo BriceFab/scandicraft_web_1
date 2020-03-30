0ef4490afcb606ddaf36c6a1c625309e1437a010
BriceFab <bricefab123@gmail.com>
2020-03-30 21:41:39 +0200

    fix register bug

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -98,7 +98,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
+            if ($targetPath =! "register_confirm") return new RedirectResponse($targetPath);
         }
 
         return new RedirectResponse($this->urlGenerator->generate('accueil'));
