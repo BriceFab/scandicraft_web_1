cb866ab2b5e4a4b5c1ad0c4a9d9692697f4fc034
BriceFab <bricefab123@gmail.com>
2020-03-31 14:51:16 +0200

    temp admin

--- assets/js/react-admin/app.js
+++ assets/js/react-admin/app.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import jsonServerProvider from 'ra-data-json-server';
-import { Admin, Resource, ListGuesser } from 'react-admin';
-import { HydraAdmin } from "@api-platform/admin";
+import { HydraAdmin, ResourceGuesser, hydraDataProvider, hydraSchemaAnalyzer } from "@api-platform/admin";
+import UserList from './entities/users/UserList';
+import UserShow from './entities/users/UserShow';
+import { fetchUtils, Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-simple-rest';
+import simpleRestProvider from 'ra-data-simple-rest';
 
-const dataProvider = jsonServerProvider('http://localhost:8000/api');
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/vnd.api+json' });
+    }
+    return fetchUtils.fetchJson(url, options);
+};
+const dataProvider = jsonServerProvider('http://localhost:8000/api', httpClient);
 
 const App = () => (
-    <HydraAdmin entrypoint="http://localhost:8000/api" />
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+    </Admin>
 );
 
 export default App;
+// <Resource name="users" list={UserList} />
 
 ReactDOM.render(<App />, document.getElementById('scandicraft_admin'));
