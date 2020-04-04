27886abbfb511ecd6c38b5f16f4cf9d35d867b84
BriceFab <bricefab123@gmail.com>
2020-04-04 02:56:37 +0200

    fix register
    - register token never expire
    - jwt error 403
    - jwt token expireAt

--- src/Service/JWTService.php
+++ src/Service/JWTService.php
@@ -9,22 +9,33 @@ use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\Parser;
 use Lcobucci\JWT\Signer\Hmac\Sha512;
 use Lcobucci\JWT\Signer\Key;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\UrlHelper;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class JWTService
 {
     private $signer;
     private $em;
     private $urlHelper;
+    private $urlGenerator;
+    private $session;
 
-    public function __construct(EntityManagerInterface $em, UrlHelper $urlHelper)
+    public function __construct(EntityManagerInterface $em, UrlHelper $urlHelper, UrlGeneratorInterface $urlGenerator, SessionInterface $session)
     {
         $this->signer = new Sha512();
         $this->em = $em;
         $this->urlHelper = $urlHelper;
+        $this->urlGenerator = $urlGenerator;
+        $this->session = $session;
     }
 
+    /**
+     * expire_after_minutes: Le temps en secondes que le token expire
+     * 0 = $_ENV['token_expiration']
+     * -1 = n'expire pas
+     */
     public function generateToken(User $user, string $action, $expire_after_minutes = 0)
     {
         $url = $this->urlHelper->getAbsoluteUrl('/');
@@ -34,43 +45,48 @@ class JWTService
             $expire_after_minutes = $_ENV['token_expiration'];
         }
 
-        $token = (new Builder())
-            ->issuedBy($url)
-            ->permittedFor($url)
-            ->issuedAt($time)
-            ->expiresAt($time + (int) $expire_after_minutes)
-            ->withClaim('user_id', $user->getId())
-            ->withClaim('action', $action)
-            ->getToken($this->signer, new Key($_ENV['jwt_sign_key']));
+        $builder = new Builder();
+        $builder->issuedBy($url);
+        $builder->permittedFor($url);
+        $builder->issuedAt($time);
+        $builder->withClaim('user_id', $user->getId());
+        $builder->withClaim('action', $action);
+        if ($expire_after_minutes > 0) {
+            $builder->expiresAt($time + (int) $expire_after_minutes);
+        }
 
-        return $token;
+        return $builder->getToken($this->signer, new Key($_ENV['jwt_sign_key']));
     }
 
     public function verifyToken($token, string $action)
     {
         //Verify if token
-        if (!$token) throw new HttpException(500, 'no token');
+        if (!$token) throw new HttpException(403, 'no token');
         $token = (new Parser())->parse((string) $token);
 
         //Verify token signed
-        if (!$token->verify($this->signer, $_ENV['jwt_sign_key'])) throw new HttpException(500, 'token invalide');
+        if (!$token->verify($this->signer, $_ENV['jwt_sign_key'])) throw new HttpException(403, 'token invalide');
 
         //Verify token time
-        if ($token->isExpired(new DateTime())) throw new HttpException(500, 'token expired');
+        if ($token->isExpired(new DateTime())) {
+            //token has expired
+            $this->session->getFlashBag()->add('error', 'Le token a expiré. Il n\'est plus valide !');
+            throw new HttpException(403, 'token invalide');
+        }
 
         //Verify issedBy
-        if (!$token->getClaim('iss')) throw new HttpException(500, 'invalide iss');
+        if (!$token->getClaim('iss')) throw new HttpException(403, 'invalide iss');
 
         //Verify action
-        if ($token->getClaim('action') !== $action) throw new HttpException(500, 'invalide action');
+        if ($token->getClaim('action') !== $action) throw new HttpException(403, 'invalide action');
 
         //Verify user
         $user_id = $token->getClaim('user_id');
-        if (!$user_id) throw new HttpException(500, 'token user invalide');
+        if (!$user_id) throw new HttpException(403, 'token user invalide');
 
         /** @var User $user */
         $user = $this->em->getRepository(User::class)->find($user_id);
-        if (!$user) throw new HttpException(500, 'user invalide');
+        if (!$user) throw new HttpException(403, 'user invalide');
 
         return $user;
     }
