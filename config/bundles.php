1d55b0493d641e59c58d9d802e474fb50e35a982
BriceFab <bricefab123@gmail.com>
2020-04-25 13:52:17 +0200

    api jwt refresh

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
