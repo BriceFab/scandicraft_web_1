8e3d99640f324264b8e7c0407b8a946baee99920
BriceFab <bricefab123@gmail.com>
2020-04-25 13:32:12 +0200

    lexik jwt auth

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
