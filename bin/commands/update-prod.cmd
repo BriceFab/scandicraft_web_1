947e78de0b7f1563e4dbff269c602fe48c0c5116
BriceFab <bricefab123@gmail.com>
2020-04-26 17:17:00 +0200

    fix migrations

--- bin/commands/update-prod.cmd
+++ bin/commands/update-prod.cmd
@@ -2,5 +2,13 @@ git status
 REM git diff <file>
 git stash
 git pull
+REM composer update --no-dev --optimize-autoloader
 REM composer install --no-dev --optimize-autoloader
-clear cache: APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+
+php bin/console doctrine:migrations:migrate
+php bin/console doctrine:migrations:execute --up 20200424184143
+php bin/console doctrine:migrations:version --add --all rem attention !If you don't want to use this workflow and instead create your schema via doctrine:schema:create, you can tell Doctrine to skip all existing migrations:
+
+php bin/console doctrine:migrations:status --show-versions
+
+APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
