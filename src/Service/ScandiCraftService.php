61f21f253c926b71ba8b1280d4b704e81075b853
BriceFab <bricefab123@gmail.com>
2020-04-28 16:25:57 +0200

    notif sondages

--- src/Service/ScandiCraftService.php
+++ src/Service/ScandiCraftService.php
@@ -3,19 +3,49 @@
 namespace App\Service;
 
 use App\Entity\MySocialmedia;
+use App\Entity\Survey;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
 
 class ScandiCraftService
 {
     private $em;
+    private $security;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, Security $security)
     {
         $this->em = $em;
+        $this->security = $security;
     }
 
     public function getSocialmedia()
     {
         return $this->em->getRepository(MySocialmedia::class)->getSocialmedia();
+    }
+
+    public function countTotalSurvey()
+    {
+        $all_survey = $this->em->getRepository(Survey::class)->findAll();
+        $enabled_sondages = array_filter($all_survey, function ($val) {
+            return $val->isEnable();
+        });
+        if (!$this->security->getUser()) {
+            return count($enabled_sondages);
+        } else {
+            $nbr_survey_not_answer = $this->countSurveysNotAnswer($all_survey);
+            return $nbr_survey_not_answer;
+        }
+    }
+
+    public function countSurveysNotAnswer($surveys)
+    {
+        $count = 0;
+        if (!$this->security->getUser()) return 0;
+        foreach ($surveys as $value) {
+            if ($value->countUserAnswers($this->security->getUser()->getId()) == 0) {
+                $count++;
+            }
+        }
+        return $count;
     }
 }
