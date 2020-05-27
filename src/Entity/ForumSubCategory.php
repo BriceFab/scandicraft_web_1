6a5a2e05f4bf7ee928695f7d92d98ff0857e38d0
BriceFab <bricefab123@gmail.com>
2020-05-27 21:25:07 +0200

    forum page sub_category

--- src/Entity/ForumSubCategory.php
+++ src/Entity/ForumSubCategory.php
@@ -27,6 +27,23 @@ class ForumSubCategory extends ForumCategory
      */
     protected $category;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $description;
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
