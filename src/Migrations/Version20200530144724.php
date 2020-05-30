ae75ff96a6a634fc23a3b0f97ed1078f6a81938e
BriceFab <bricefab123@gmail.com>
2020-05-30 16:48:44 +0200

    ready for deploy forum

--- src/Migrations/Version20200530144724.php
+++ src/Migrations/Version20200530144724.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200528194627 extends AbstractMigration
+final class Version20200530144724 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -27,7 +27,7 @@ final class Version20200528194627 extends AbstractMigration
         $this->addSql('CREATE TABLE forum_category (id INT AUTO_INCREMENT NOT NULL, created_by_id INT NOT NULL, name VARCHAR(255) NOT NULL, priority SMALLINT DEFAULT NULL, created_at DATETIME NOT NULL, active TINYINT(1) NOT NULL, slug VARCHAR(255) NOT NULL, entity_type VARCHAR(255) NOT NULL, INDEX IDX_21BF9426B03A8386 (created_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE forum_discussion (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, sub_category_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, pin TINYINT(1) DEFAULT \'0\' NOT NULL, archive TINYINT(1) DEFAULT \'0\' NOT NULL, created_at DATETIME NOT NULL, staff_only TINYINT(1) DEFAULT \'0\' NOT NULL, slug VARCHAR(255) NOT NULL, INDEX IDX_428F444AB03A8386 (created_by_id), INDEX IDX_428F444AF7BFE87C (sub_category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE forum_discussion_answer (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, discussion_id INT DEFAULT NULL, created_at DATETIME NOT NULL, message LONGTEXT NOT NULL, INDEX IDX_55F7781EB03A8386 (created_by_id), INDEX IDX_55F7781E1ADED311 (discussion_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE forum_sub_category (id INT NOT NULL, category_id INT NOT NULL, writable TINYINT(1) DEFAULT \'1\' NOT NULL, description LONGTEXT DEFAULT NULL, accept_staff_only TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_843D01DF12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE forum_sub_category (id INT NOT NULL, category_id INT NOT NULL, writable TINYINT(1) DEFAULT \'1\' NOT NULL, description LONGTEXT DEFAULT NULL, accept_staff_only TINYINT(1) DEFAULT \'0\' NOT NULL, sub_title VARCHAR(255) DEFAULT NULL, INDEX IDX_843D01DF12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_ip (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, ip VARCHAR(255) NOT NULL, login_at DATETIME NOT NULL, INDEX IDX_BDB407E8A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE action_log ADD CONSTRAINT FK_B2C5F685A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE exception_log ADD CONSTRAINT FK_3A3168C1A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
