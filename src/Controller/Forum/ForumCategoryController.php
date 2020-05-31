c331575671baec93eeeb02bb5ed419ca2a5b4c3c
BriceFab <bricefab123@gmail.com>
2020-05-31 22:47:20 +0200

    forum seo change, robots, sitemap, forum message limit

--- src/Controller/Forum/ForumCategoryController.php
+++ src/Controller/Forum/ForumCategoryController.php
@@ -23,6 +23,22 @@ class ForumCategoryController extends ForumController
         ]);
     }
 
+    // /**
+    //  * @Route("/forum/{main_slug}", name="forum_show_category_detail")
+    //  * @ParamConverter("category", options={"mapping": {"main_slug": "slug"}})
+    //  */
+    // public function showCategoryDetail(Request $request, ForumCategory $category, ForumSubCategory $subCategory)
+    // {
+    //     if (!$category->getActive() || !$subCategory->getActive()) {
+    //         return $this->retirectToPreviousRoute($request, 'Forum: cette catégorie n\'est plus active', ForumController::$default_route);
+    //     }
+
+    //     return $this->render('forum/show_subcategories.html.twig', [
+    //         'forumCategory' => $category,
+    //         'forumSubCategory' => $subCategory
+    //     ]);
+    // }
+
     /**
      * @Route("/forum/{main_slug}/{sub_slug}", name="forum_show_subcategories")
      * @ParamConverter("category", options={"mapping": {"main_slug": "slug"}})
