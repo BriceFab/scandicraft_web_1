1854c86c7d13f6558091aef64c917d40dda1c8c5
BriceFab <bricefab123@gmail.com>
2020-06-14 17:22:53 +0200

    home show spoils missing share

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SpoilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,18 +11,23 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index()
+    public function index(SpoilRepository $spoilRepository)
     {
-        return $this->render('home/index.html.twig');
+        $last = $spoilRepository->findOneBy([], ['createdAt' => 'DESC']);
+        $missing_share = $last->getShareGoal() - $last->getCurrentShare();
+
+        return $this->render('home/index.html.twig', [
+            'last_spoil_missing_number' => $missing_share > 0 ? $missing_share : 0
+        ]);
     }
 
     /**
      * @Route("/jouer", name="jouer")
      * @Route("/play")
      * @Route("/join")
-    */
-    public function jouer() {
+     */
+    public function jouer()
+    {
         return $this->render('jouer/index.html.twig');
     }
-
 }
