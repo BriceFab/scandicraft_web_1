4f669db9c603d0f8b41fe23c6893597f1596c4ea
BriceFab <bricefab123@gmail.com>
2020-08-29 23:59:44 +0200

    twig html_component + paypal disable card

--- assets/javascript/payments/PaymentPaypal.jsx
+++ assets/javascript/payments/PaymentPaypal.jsx
@@ -5,9 +5,9 @@ import {CONFIG} from "../_common/config";
 class PaymentPaypal extends PureComponent {
     componentDidMount() {
         const {element_ref} = this.props;
-        const {CLIENT_ID, CURRENCY, INTENT} = CONFIG.PAYMENTS.PAYPAL;
+        const {CLIENT_ID, CURRENCY, INTENT, DISABLE_FUNDING} = CONFIG.PAYMENTS.PAYPAL;
 
-        injectHeadScript(`https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}&currency=${CURRENCY}&intent=${INTENT}`, () => {
+        injectHeadScript(`https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}&currency=${CURRENCY}&intent=${INTENT}&disable-funding=${DISABLE_FUNDING}`, () => {
             paypal.Buttons({
                 createOrder: (data, actions) => {
                     // This function sets up the details of the transaction, including the amount and line item details.
