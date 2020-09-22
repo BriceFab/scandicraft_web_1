fd96454ca3629b743c743423537f735ce810e444
BriceFab <bricefab123@gmail.com>
2020-09-22 21:54:29 +0200

    boutique categories + articles

--- src/Controller/Store/StoreController.php
+++ src/Controller/Store/StoreController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Store;
 
+use App\Repository\StoreArticleRepository;
+use App\Repository\StoreCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StoreController extends AbstractController
@@ -10,9 +13,15 @@ class StoreController extends AbstractController
     /**
      * @Route("/boutique", name="store")
      * @Route("/store")
+     * @param StoreCategoryRepository $categoriesRepository
+     * @param StoreArticleRepository $articlesRepository
+     * @return Response
      */
-    public function boutiquePage()
+    public function boutiquePage(StoreCategoryRepository $categoriesRepository, StoreArticleRepository $articlesRepository)
     {
-        return $this->render('store/index.html.twig');
+        return $this->render('store/index.html.twig', [
+            'categories' => $categoriesRepository->findBy(['enable' => true]),
+            'articles' => $articlesRepository->findBy(['enable' => true]),
+        ]);
     }
 }
--- src/Migrations/Version20200922193111.php
+++ src/Migrations/Version20200922193111.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200725174226 extends AbstractMigration
+final class Version20200922193111 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,12 +22,11 @@ final class Version20200725174226 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE attachment (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, created_at DATETIME NOT NULL, url VARCHAR(255) NOT NULL, alt VARCHAR(255) DEFAULT NULL, INDEX IDX_795FD9BBB03A8386 (created_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE spoil_attachment (spoil_id INT NOT NULL, attachment_id INT NOT NULL, INDEX IDX_13ADC4DD2EE5651B (spoil_id), INDEX IDX_13ADC4DD464E68B (attachment_id), PRIMARY KEY(spoil_id, attachment_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BBB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE spoil_attachment ADD CONSTRAINT FK_13ADC4DD2EE5651B FOREIGN KEY (spoil_id) REFERENCES spoil (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE spoil_attachment ADD CONSTRAINT FK_13ADC4DD464E68B FOREIGN KEY (attachment_id) REFERENCES attachment (id) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE store_article (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, duration INT DEFAULT NULL, enable TINYINT(1) NOT NULL, price INT NOT NULL, until_date DATETIME DEFAULT NULL, slug VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_1B322F81B03A8386 (created_by_id), INDEX IDX_1B322F8112469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE store_article ADD CONSTRAINT FK_1B322F81B03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE store_article ADD CONSTRAINT FK_1B322F8112469DE2 FOREIGN KEY (category_id) REFERENCES store_category (id)');
         $this->addSql('ALTER TABLE action_log CHANGE user_id user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE attachment CHANGE created_by_id created_by_id INT DEFAULT NULL, CHANGE alt alt VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE dev_progression CHANGE under_maintenance under_maintenance TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE exception_log CHANGE user_id user_id INT DEFAULT NULL, CHANGE method method VARCHAR(255) DEFAULT NULL, CHANGE exception_code exception_code SMALLINT DEFAULT NULL, CHANGE ip ip VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE forum_category CHANGE priority priority SMALLINT DEFAULT NULL');
@@ -50,10 +49,10 @@ final class Version20200725174226 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE spoil_attachment DROP FOREIGN KEY FK_13ADC4DD464E68B');
-        $this->addSql('DROP TABLE attachment');
-        $this->addSql('DROP TABLE spoil_attachment');
+        $this->addSql('ALTER TABLE store_article_attachment DROP FOREIGN KEY FK_333B5EE85C7DDD71');
+        $this->addSql('DROP TABLE store_article');
         $this->addSql('ALTER TABLE action_log CHANGE user_id user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE attachment CHANGE created_by_id created_by_id INT DEFAULT NULL, CHANGE alt alt VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE dev_progression CHANGE under_maintenance under_maintenance TINYINT(1) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE exception_log CHANGE user_id user_id INT DEFAULT NULL, CHANGE method method VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE exception_code exception_code SMALLINT DEFAULT NULL, CHANGE ip ip VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE forum_category CHANGE priority priority SMALLINT DEFAULT NULL');
