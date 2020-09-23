64e784ced390fbc9dfe2b991fec6c43c85250127
BriceFab <bricefab123@gmail.com>
2020-09-23 20:14:48 +0200

    stripe v1 (accept payment)

--- assets/javascript/_common/dynamic_component/dynamic_components.js
+++ assets/javascript/_common/dynamic_component/dynamic_components.js
@@ -1,9 +1,11 @@
 import PaymentPaypal from "../../payments/types/PaymentPaypal";
 import PaymentDedipass from "../../payments/types/PaymentDedipass";
 import CrediterSteps from "../../payments/CrediterSteps";
+import PaymentStripe from "../../payments/types/PaymentStripe";
 
 export const DYNAMIC_COMPONENTS = {
     'payment_paypal': PaymentPaypal,
     'payment_dedipass': PaymentDedipass,
+    'payment_stripe': PaymentStripe,
     'crediter_steps': CrediterSteps,
 };
