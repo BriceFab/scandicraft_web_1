8b9209fb97fa93128421c6d9f44e32f6da3beaf2
BriceFab <bricefab123@gmail.com>
2020-10-11 15:32:24 +0200

    test captcha v3

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,14 +6,13 @@ use App\Entity\User;
 use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
@@ -60,7 +59,7 @@ class RegistrationFormType extends AbstractType
                 'type' => PasswordType::class,
                 'required' => true,
                 'mapped' => false,
-                'first_options'  => ['label' => 'input.password', 'error_bubbling' => true],
+                'first_options' => ['label' => 'input.password', 'error_bubbling' => true],
                 'second_options' => ['label' => 'input.password.repeated', 'error_bubbling' => true],
                 'invalid_message' => 'The password fields must match.',
                 'constraints' => [
@@ -75,9 +74,15 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('captcha', Recaptcha3Type::class, [
-                'constraints' => new Recaptcha3(),
-                'action_name' => 'register',
-            ]);
+                'constraints' => new Recaptcha3(['message' => 'There were problems with your captcha. Please try again or contact with support and provide following code(s): {{ errorCodes }}']),
+                'action_name' => 'homepage',
+                'label' => 'captcha',
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'button'],
+                'label' => 'submit.register',
+            ])
+        ;
         // ->add('agreeTerms', CheckboxType::class, [
         //     'mapped' => false,
         //     'constraints' => [
